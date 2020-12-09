2caa8416ba0089f506541e9d9f35e0cb49962621
shreyas k s <shreyas4156@gmail.com>
2020-12-09 20:15:49 +0530

    inital commit

---
+++ public/precache-manifest.55c26b1c2cee9dc548f153534d6001d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46264c51a03286487e0bfa5394f2649b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4bbb9efe67944d062f1",
+    "url": "/static/css/2.178e3b40.chunk.css"
+  },
+  {
+    "revision": "dde7035335b08af5f75b",
+    "url": "/static/css/main.e26999f3.chunk.css"
+  },
+  {
+    "revision": "d4bbb9efe67944d062f1",
+    "url": "/static/js/2.181c2a4b.chunk.js"
+  },
+  {
+    "revision": "3e533a1a18939928aa484d9457ec0229",
+    "url": "/static/js/2.181c2a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde7035335b08af5f75b",
+    "url": "/static/js/main.897a6da6.chunk.js"
+  },
+  {
+    "revision": "bcc57a452636eafc0232",
+    "url": "/static/js/runtime-main.6cb9de01.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
